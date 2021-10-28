4c33e2f4013405d6eb0b2f5caf935946c633d6eb
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    comet: Add privapp permissions for EuiccGoogle
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

--- device-comet.mk
+++ device-comet.mk
@@ -17,6 +17,10 @@ SHIPPING_API_LEVEL := 34
 # Inherit from zumapro
 include device/google/zumapro/device-shipping-common.mk
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
 # Fold extensions
 PRODUCT_SYSTEM_SERVER_JARS += \
     system_ext:comet-services
