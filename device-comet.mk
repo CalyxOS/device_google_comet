bf4a626a73d28320f4b71d0265baf9070b8a3ed2
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    comet: Add privapp permissions for EuiccGoogle
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

--- device-comet.mk
+++ device-comet.mk
@@ -21,6 +21,10 @@ include device/google/zumapro/common.mk
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
 # Fold extensions
 PRODUCT_SYSTEM_SERVER_JARS += \
     system_ext:comet-services
