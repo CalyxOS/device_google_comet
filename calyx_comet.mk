a6606d66adcc46949db91301c0d37e3ee9b48c9f
Michael Bestas <mkbestas@gmail.com>
2025-03-06 03:22:57 +0200

    Update fingerprint/build description from BP1A.250305.020
    Change-Id: Iabd53b0011bb550bd84724806bb0bb4765e295b9

--- calyx_comet.mk
+++ calyx_comet.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := Pixel 9 Pro Fold
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc="comet-user 15 AP4A.250205.002 12821496 release-keys" \
-    BuildFingerprint=google/comet/comet:15/AP4A.250205.002/12821496:user/release-keys \
+    BuildDesc="comet-user 15 BP1A.250305.020 13009785 release-keys" \
+    BuildFingerprint=google/comet/comet:15/BP1A.250305.020/13009785:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
