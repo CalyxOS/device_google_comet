aa5fc0d696dbf1c92b151844473f399a13c591e1
Michael Bestas <mkbestas@gmail.com>
2026-03-20 00:23:52 +0200

    comet: Remove unwanted overlays
    We don't ship the proprietary google packages for these.
    
    Change-Id: I0bd69364411626a1574afce9aabe5880972b6d51

--- device-comet.mk
+++ device-comet.mk
@@ -40,10 +40,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGES += \
     ConnectivityResourcesOverlayCometOverride \
-    DMServiceOverlayVendorComet \
     FrameworkResOverlayProductComet \
     FrameworkResOverlayVendorComet \
-    GRilServiceOverlay_CT3 \
     LargeScreenConfigOverlay \
     LargeScreenSettingsProviderOverlay \
     NexusLauncherReleaseOverlayVendorComet \
