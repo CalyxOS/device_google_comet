CalyxOS/device_google_tegu

Initial CalyxOS makefile
Change-Id: I0aeb85302967c3ce004f7f18a967813684783088

AndroidProducts.mk
@@ -5,4 +5,5 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_tegu.mk \
+    $(LOCAL_DIR)/calyx_tegu.mk \
     $(LOCAL_DIR)/lineage_tegu.mk

CalyxOS/device_google_comet

AndroidProducts.mk
@@ -5,4 +5,5 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_comet.mk \
+    $(LOCAL_DIR)/calyx_comet.mk \
     $(LOCAL_DIR)/lineage_comet.mk